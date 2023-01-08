20b7749555450551a935c791350ea4dd5be654bd
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-01-08 19:54:52 +0530

    Add: Comment for all 8 commands

--- BrainFuckInterpreter.java
+++ BrainFuckInterpreter.java
@@ -2,3 +2,18 @@ public class BrainFuckInterpreter {
     public static void main(String[] args) {
     }
 }
+
+
+/*
+    All 8 commands of BrainFuck for reference:
+
+    >    Increment the pointer (to point to the next cell to the right).
+    <    Decrement the pointer (to point to the next cell to the left).
+    +    Increment the value at the pointer.
+    -    Decrement the value at the pointer.
+    .    Output the value at the pointer as a character.
+    ,    Input a character and store it at the pointer.
+    [    If the value at the pointer is zero, jump to the corresponding ]
+    ]    If the value at the pointer is nonzero, jump back to the corresponding [
+
+*/
