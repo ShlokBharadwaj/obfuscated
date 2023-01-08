c8cf0bc6bff231f162d5227af08f3baefce3c440
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-01-08 19:59:36 +0530

    Add: Initialize the interpreter's variables

--- BrainFuckInterpreter.java
+++ BrainFuckInterpreter.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
+
 public class BrainFuckInterpreter {
     public static void main(String[] args) {
-        String program = "+++++++++++++[>+++++++++++[>++++++++++>+++>++++<<-]>+.>+[<]<-]>>++++.>+++++++[<++++++>-]<.>>>>[<+>-]<---.+++[<+++>-]<.<<<<.+++[<+++>-]<.>.+++[<++>-]<.++++++++++.";
+
+        // String to be interpreted
+        String exec = "+++++++++++++[>+++++++++++[>++++++++++>+++>++++<<-]>+.>+[<]<-]>>++++.>+++++++[<++++++>-]<.>>>>[<+>-]<---.+++[<+++>-]<.<<<<.+++[<+++>-]<.>.+++[<++>-]<.++++++++++.";
+
+        byte[] memory = new byte[30001];
+        int pointer = 0;
     }
 }
 
-
 /*
-    All 8 commands of BrainFuck for reference:
-
-    >    Increment the pointer (to point to the next cell to the right).
-    <    Decrement the pointer (to point to the next cell to the left).
-    +    Increment the value at the pointer.
-    -    Decrement the value at the pointer.
-    .    Output the value at the pointer as a character.
-    ,    Input a character and store it at the pointer.
-    [    If the value at the pointer is zero, jump to the corresponding ]
-    ]    If the value at the pointer is nonzero, jump back to the corresponding [
-
-*/
+ * All 8 commands of BrainFuck for reference:
+ * 
+ * > Increment the pointer (to point to the next cell to the right).
+ * < Decrement the pointer (to point to the next cell to the left).
+ * + Increment the value at the pointer.
+ * - Decrement the value at the pointer.
+ * . Output the value at the pointer as a character.
+ * , Input a character and store it at the pointer.
+ * [ If the value at the pointer is zero, jump to the corresponding ]
+ * ] If the value at the pointer is nonzero, jump back to the corresponding [
+ * 
+ */
